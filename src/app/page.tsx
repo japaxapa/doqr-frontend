0ee3a8e433ca5b2046266b69b8278e88df45d20c
Mario Kawakita <mario.jhk@gmail.com>
2026-04-17 11:57:59 -0300

    feat: home structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,78 @@
-import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
+import { Field } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Plus } from "lucide-react";
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <div
+      id="employees-page"
+      className="flex justify-center"
+    >
+      <div
+        id="employees-container"
+        className="flex flex-col flex-1 max-w-7xl"
+      >
+        <div
+          id="employees-title"
+          className="flex flex-col py-8 gap-1"
+        >
+          <h1
+            id="employees-title-main"
+            className="font-bold text-4xl"
+          >
+            Controle de Funcionários
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          <h2
+            id="employees-title-sub"
+            className="font-bold text-xl"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            Empresa DoQR Tecnologia
+          </h2>
         </div>
-      </main>
+
+        <div
+          id="employees-content"
+          className="flex flex-col gap-4"
+        >
+          <div
+            id="employees-content-search"
+            className="flex flex-1 flex-row justify-between items-center"
+          >
+            <Field
+              orientation="horizontal"
+              className="flex flex-1 justify-between"
+            >
+              <Input
+                className="max-w-sm"
+                placeholder="Buscar Funcionário..."
+              />
+              <Button>
+                <Plus /> Novo Funcionário
+              </Button>
+            </Field>
+          </div>
+          <div
+            id="employees-content-table"
+            className="flex flex-1 flex-row justify-between items-center"
+          >
+            <Table>
+              <TableHeader className="bg-table-title">
+                <TableRow>
+                  <TableHead>Nome</TableHead>
+                  <TableHead>E-mail</TableHead>
+                  <TableHead>CPF</TableHead>
+                  <TableHead>Celular</TableHead>
+                  <TableHead>Data de Nascimento</TableHead>
+                  <TableHead>Tipo de Contratação</TableHead>
+                  <TableHead>Status</TableHead>
+                  <TableHead>Ação</TableHead>
+                </TableRow>
+              </TableHeader>
+            </Table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
